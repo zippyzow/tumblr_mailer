9b6fa5f6fba6afbffdd0d6ca46919bb356552c39
Hailey Keen <haileykeen@Haileys-MacBook-Pro.local>
2016-01-08 16:17:50 -0500

    finished

--- tumblr_mailer.js
+++ tumblr_mailer.js
@@ -1,14 +1,48 @@
 /**
  * Created by haileykeen on 1/6/16.
  */
+
 var fs = require('fs');
 var ejs = require('ejs');
 var tumblr = require('tumblr.js');
 
-var csvFile = fs.readFileSync("friend_list.csv","utf8");
-var emailTemplate = fs.readFileSync('email_template.html', 'utf8');
+//this is needed to make tumblr api requests
+var client = tumblr.createClient({
+  consumer_key: 'xxxxx',
+  consumer_secret: 'xxxxx',
+  token: 'xxxxx',
+  token_secret: 'xxxxx'
+});
 
+//
+var emailTemplate = fs.readFileSync('email_template.ejs', 'utf8');
+
+var csvFile = fs.readFileSync('friend_list.csv', 'utf8');
 var contacts = csvParse(csvFile);
+
+client.posts('haileyckeen', function(err, blog) {
+  //filter the posts array with the timestamp
+  var timestampFromSevenDaysAgo = getTimestampFromSevenDaysAgo();
+  var latestPosts = blog.posts.filter(function(post) {
+    return post.timestamp >= timestampFromSevenDaysAgo;
+  });
+
+  //create a new array of rendered emails from the contact lists
+  var renderedEmails = contacts.map(function(contact) {
+    return renderEmail(contact, latestPosts);
+  });
+
+  console.log(renderedEmails);
+});
+
+function getTimestampFromSevenDaysAgo() {
+  //get timestamp from 7 days ago
+  var numSecondsInSevenDays = 7 * 24 * 60 * 60;
+  var date = new Date();
+  //all timestamps are in seconds
+  var currentTimestamp = date.getTime() / 1000;
+  return currentTimestamp - numSecondsInSevenDays;
+}
 
 function csvParse(csvFile) {
   var allContacts = [];
@@ -25,14 +59,11 @@ function csvParse(csvFile) {
   return allContacts;
 }
 
-//Replacing FIRST_NAME and NUM_MONTHS_SINCE_CONTACT in email_template
-contacts.forEach(function(contact) {
-  var firstName = contact["firstName"];
-  var numMonthsSinceContact = contact["numMonthsSinceContact"];
-
-  var templateCopy = emailTemplate
-      .replace(/FIRST_NAME/gi, firstName)
-      .replace(/NUM_MONTHS_SINCE_CONTACT/gi, numMonthsSinceContact);
-
-  console.log(templateCopy);
-});
+function renderEmail(contact, latestPosts) {
+  var data = {
+    firstName: contact["firstName"],
+    numMonthsSinceContact: contact["numMonthsSinceContact"],
+    latestPosts: latestPosts
+  };
+  return ejs.render(emailTemplate, data);
+}
